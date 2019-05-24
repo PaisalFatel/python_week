00777356c8159db452f195608f77afdd487603c2
PaisalFatel <faisalpatel7@gmail.com>
2019-05-24 10:04:16 +0100

    (pythonweek)

---
+++ Subjectcalc.py
@@ -0,0 +1,6 @@
+Physics=80
+Chemistry=100
+Mathematics=100
+Total=Physics+Chemistry+Mathematics
+print("The grand total is")
+print(Total)
---
+++ subjectcalc2.py
@@ -0,0 +1,12 @@
+Physics=80
+Chemistry=100
+Maths=100
+Total=Physics+Chemistry+Maths
+per=Total*(100/450)
+print("Physics:",Physics)
+print("Chemistry:",Chemistry)
+print("Maths:",Maths)
+print("-------------------------------")
+print("Total Marks:",Total)
+print("Percentage:",per)
+print("-------------------------------")
